049e378f341d7f171c11fdf36cade0d4200b20d2
dongze.xu <dongze.xu@happyelements.com>
2022-06-05 13:43:26 +0800

    add a bool holder

---
+++ src/main/java/com/xdz/web/dsa/holder/BoolHolder.java
@@ -0,0 +1,18 @@
+package com.xdz.web.dsa.holder;
+
+/**
+ * Description: bool value reference holder<br/>
+ * Author: dongze.xu<br/>
+ * Date: 2022/6/5 13:27<br/>
+ * Version: 1.0<br/>
+ */
+public class BoolHolder {
+    public boolean value;
+
+    public BoolHolder() {
+    }
+
+    public BoolHolder(boolean value) {
+        this.value = value;
+    }
+}
