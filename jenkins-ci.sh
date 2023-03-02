bd6d0c8c19cd052dcb9e0a95ade86b9466580e3c
dongze.xu <1146520765@qq.com>
2023-03-02 23:36:57 +0800

    ci

--- jenkins-ci.sh
+++ jenkins-ci.sh
@@ -3,6 +3,10 @@
 #git clone -b main https://github.com./XuDongZe/web.git/
 #cd web
 
+# add nginx.conf
+rm -f /var/lib/docker/volumes/nginx/nginx.conf
+cp nginx.conf /var/lib/docker/volumes/nginx/nginx.conf
+
 docker build . -t xudongze/tomcat-springmvc-web:latest
 docker container stop myweb && docker container rm myweb
 docker run -d --name myweb --network server-web xudongze/tomcat-springmvc-web:latest
