0ea3594fa89a8c5122e2e66c2779c79dfc1a7fef
dongze.xu <1146520765@qq.com>
2023-03-02 23:39:06 +0800

    ci

--- jenkins-ci.sh
+++ jenkins-ci.sh
@@ -6,7 +6,10 @@
 # add nginx.conf
 rm -f /var/lib/docker/volumes/nginx/nginx.conf
 cp nginx.conf /var/lib/docker/volumes/nginx/nginx.conf
+# docker-compose 启动
+docker-compose down
+docker-compose up -d
 
-docker build . -t xudongze/tomcat-springmvc-web:latest
-docker container stop myweb && docker container rm myweb
-docker run -d --name myweb --network server-web xudongze/tomcat-springmvc-web:latest
+#docker build . -t xudongze/tomcat-springmvc-web:latest
+#docker container stop myweb && docker container rm myweb
+#docker run -d --name myweb --network server-web xudongze/tomcat-springmvc-web:latest
